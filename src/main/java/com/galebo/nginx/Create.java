c3eb1236d10046180c50fead9fed87eab90bb8b4
galebo <galebo@163.com>
2013-12-24 22:42:25 +0800

    add some log

--- src/main/java/com/galebo/nginx/Create.java
+++ src/main/java/com/galebo/nginx/Create.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 
 import com.galebo.nginx.Module.Parameter;
 
@@ -16,29 +17,35 @@ import freemarker.template.Template;
 
 public class Create {
 	public static void main(String[] args) throws IOException {
-		Module module=new Module();
+		Logger log = Logger.getLogger(Create.class);
+		log.info("start");
+		
+		Module module = new Module();
 		module.setName("concat");
-		module.addParameter(new Parameter("enable",false));
-		module.addParameter(new Parameter("max_files",10));
-		module.addParameter(new Parameter("unique",true));
-		module.addParameter(new Parameter("delimiter",""));
-		module.addParameter(new Parameter("ignore_file_error",false));
-		module.addParameter(new Parameter("types",new String[]{"application/x-javascript","text/css"}));
-		FileUtils.writeStringToFile(new File("/WORK/git/tengine/src/http/modules/ngx_http_"+module.getName()+"_module1.c"), genFtlResult("module_handle.ftl", module));
+		module.addParameter(new Parameter("enable", false));
+		module.addParameter(new Parameter("max_files", 10));
+		module.addParameter(new Parameter("unique", true));
+		module.addParameter(new Parameter("delimiter", ""));
+		module.addParameter(new Parameter("ignore_file_error", false));
+		module.addParameter(new Parameter("types", new String[] { "application/x-javascript", "text/css" }));
+
+		FileUtils.writeStringToFile(new File("/WORK/git/tengine/src/http/modules/ngx_http_" + module.getName() + "_module1.c"), genFtlResult("module_handle.ftl", module));
+
+		log.info("end");
 	}
-	
-	static  String genFtlResult(String ftlName, Object inputInfo) {
+
+	static String genFtlResult(String ftlName, Object inputInfo) {
 		try {
 			Configuration freemarkerCfg = new Configuration();
 			freemarkerCfg.setDirectoryForTemplateLoading(new File("ftl"));
 			freemarkerCfg.setEncoding(Locale.getDefault(), "UTF-8");
 			Locale.setDefault(Locale.ENGLISH);
-	
+
 			Template template = freemarkerCfg.getTemplate(ftlName);
 			template.setEncoding("UTF-8");
-	
+
 			StringWriter writer = new StringWriter();
-			Map<String,Object> map=new HashMap<String, Object>();
+			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("bean", inputInfo);
 			template.process(map, writer);
 			return writer.toString();
